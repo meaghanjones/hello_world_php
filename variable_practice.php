055f9080a4590bcf5e01d36f23b0a2aeac701f09
meaghanjones <meaghan.m.jones@gmail.com>
2016-12-13 15:22:32 -0200

    complete php variable practice from epicodus online lessons

---
+++ variable_practice.php
@@ -0,0 +1,18 @@
+<?php
+    $friend_name = "Sonja";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Greetings!</title>
+</head>
+<body>
+    <h1>Greetings From Afar</h1>
+    <p>Dear <?php echo $friend_name; ?>,</p>
+    <p>How are you? I hope that you are having a nice weekend. I'm vacationing in Brazil while I learn programming! </p>
+    <p><?php echo $friend_name; ?>, you would not believe how hot it is here. I should have gone to Alaska instead. But I like programming a lot, so I've got that going for me. </p>
+    <p>Looking forward to seeing you soon. I'll bring you back a souvenir. </p>
+    <p>Cheers,</p>
+    <p>Meaghan</p>
+</body>
+</html>
